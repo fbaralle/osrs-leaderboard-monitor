0605f9ceb83068555e56a8c1e8179ccbb89b4ebd
Francisco Baralle <fbaralle93@gmail.com>
2025-11-06 20:20:52 -0300

    remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IndexerModule } from './common/indexer/indexer.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -19,7 +17,5 @@ import { ApiModule } from './common/api/api.module';
     CacheModule.register({ isGlobal: true }),
     ScheduleModule.forRoot(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
